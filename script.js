c39de27a1805dde58eaa08419391a22856e6a427
Muhammad Saeed Jangda <98708732+MuhammadSaeedJangda@users.noreply.github.com>
2023-02-01 09:38:38 +0500

    Calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+
+     else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'x'){
+      string = string.slice(0, -1);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == '%'){
+      string = (eval(string) / 100).toString();
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == '2x'){
+      string = (eval(string) * eval(string)).toString();
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
